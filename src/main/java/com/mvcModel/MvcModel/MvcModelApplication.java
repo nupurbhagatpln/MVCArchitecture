1b4c11371db3385f30198690f3e40a08b651362d
nupur <nupurbhagatpln@gmail.com>
2025-12-12 17:58:21 +0530

    structure of the MVC Archeitecture

---
+++ src/main/java/com/mvcModel/MvcModel/MvcModelApplication.java
@@ -0,0 +1,13 @@
+package com.mvcModel.MvcModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvcModel/MvcModel/MvcModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvcModel.MvcModel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
