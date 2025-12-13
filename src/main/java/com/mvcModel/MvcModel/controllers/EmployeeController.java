9ec773075032dbc52714c8a9e54841c07afb8c69
nupur <nupurbhagatpln@gmail.com>
2025-12-13 13:13:32 +0530

    connecting it to h2 memory database and performing post, get mapping

--- src/main/java/com/mvcModel/MvcModel/controllers/EmployeeController.java
+++ src/main/java/com/mvcModel/MvcModel/controllers/EmployeeController.java
@@ -1,33 +1,42 @@
 package com.mvcModel.MvcModel.controllers;
 
 import com.mvcModel.MvcModel.dtos.EmployeeDto;
+import com.mvcModel.MvcModel.entities.Employee;
+import com.mvcModel.MvcModel.repositories.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/employee")
+@RequiredArgsConstructor
 public class EmployeeController {
 
+    private final EmployeeRepository employeeRepository;
+
     @GetMapping(path="/{employeeId}")
-    public EmployeeDto getEmpolyeeById(@PathVariable(name="employeeId") Long id)
+    public Employee getEmpolyeeById(@PathVariable(name="employeeId") Long id)
     {
-        return new EmployeeDto(id,"Nupur","nupur@gmail.com",23, LocalDate.of(2000,2,24),true);
+//        return new EmployeeDto(id,"Nupur","nupur@gmail.com",23, LocalDate.of(2000,2,24),true);
+        return  employeeRepository.findById(id).orElse(null);
     }
 
     // request param is to pass some values in the url to perform sortinf and filtering the data, and it can be optional
     @GetMapping(path="")
-    public String getAllEmployee(@RequestParam(required = false) Integer age, @RequestParam(required = false) String str)
+    public List<Employee> getAllEmployee(@RequestParam(required = false) Integer age, @RequestParam(required = false) String str)
     {
-        return "All Employee"+age+" "+str;
+        //        return "All Employee"+age+" "+str;
+        return employeeRepository.findAll();
     }
 
     // Request Body is to pass the whole object as a param in controller
     @PostMapping("")
-    public EmployeeDto createANewEmployee(@RequestBody EmployeeDto employeeDto)
+    public Employee createANewEmployee(@RequestBody Employee employee)
     {
-        employeeDto.setId(120L);
-        return  employeeDto;
+//        employeeDto.setId(120L);
+        return  employeeRepository.save(employee);
     }
 
 }
---
+++ src/main/java/com/mvcModel/MvcModel/entities/Employee.java
@@ -0,0 +1,31 @@
+package com.mvcModel.MvcModel.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfJoining;
+    private Boolean isActive;
+
+
+}
